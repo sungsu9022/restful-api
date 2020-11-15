5d133ee0d57abf00bece806ed52844656c137593
sungsu.park <sungsu.park@navercorp.com>
2020-11-15 19:35:54 +0900

    [#] add localdatetime serializer & deserializer

--- src/main/java/com/sungsu/dev/config/ApplicationConfig.java
+++ src/main/java/com/sungsu/dev/config/ApplicationConfig.java
@@ -1,13 +1,17 @@
 package com.sungsu.dev.config;
 
+import java.time.LocalDateTime;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.sungsu.dev.app.common.serializer.LocalDateTimeDeserializer;
+import com.sungsu.dev.app.common.serializer.LocalDateTimeSerializer;
 
 @Configuration
 public class ApplicationConfig {
@@ -17,11 +21,19 @@ public class ApplicationConfig {
 		final ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json()
 			.featuresToDisable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
 			.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-			.modules(new JavaTimeModule())
 			.build();
 
-		objectMapper.setSerializationInclusion(Include.NON_NULL);
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.registerModule(localDateTimeModule());
 		return objectMapper;
+	}
+
+	@Bean(name = "localDateTimeModule")
+	public SimpleModule localDateTimeModule() {
+		final SimpleModule module = new SimpleModule();
+		module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+		module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+		return module;
 	}
 
 }
